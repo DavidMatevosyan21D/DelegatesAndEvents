707e74d435fb387fddd5de18312b50c7070cd540
DavidMatevosyan21D <113889057+DavidMatevosyan21D@users.noreply.github.com>
2023-04-06 19:33:55 +0300

    Add files via upload

---
+++ Timer/Timer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Timer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите часы: ");
+            int hours = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите минуты: ");
+            int minutes = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите секунды: ");
+            int seconds = int.Parse(Console.ReadLine());
+
+            Timer timer = new Timer(hours, minutes, seconds);
+            timer.Start();
+        }
+    }
+}
---
+++ Timer/Timer/Timer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+namespace Timer
+{
+    class Timer
+    {
+        private int Hours;
+        private int Minutes;
+        private int Seconds;
+
+        public Timer(int hours, int minutes, int seconds)
+        {
+            Hours = hours;
+            Minutes = minutes;
+            Seconds = seconds;
+        }
+
+        public void Start()
+        {
+            int totalSeconds = Hours * 3600 + Minutes * 60 + Seconds;
+            Thread.Sleep(totalSeconds * 1000);
+            Console.WriteLine("Таймер завершен");
+        }
+    }
+}
